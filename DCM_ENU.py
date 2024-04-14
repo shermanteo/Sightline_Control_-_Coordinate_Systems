975806dcc1c0a4da3fe3075395abb9875cffa6b1
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-04-14 19:47:26 +0800

    Add files via upload

---
+++ DCM_ENU.py
@@ -0,0 +1,20 @@
+# Direction Cosine Matrix (DCM)
+# Rotation of the ECEF Coordinates to the Local Vertical Coordinates
+# The rotation matrix will be rotating the coordinates in East-North-Up (ENU) Convention
+
+import numpy as np
+from math import sin, cos
+
+
+Latitude1 = float(input('Latitude of the target position:'))        # Latitude of the target position
+Longitude1 = float(input('Longitude of the target position:'))      # Longitude of the target position
+Altitude1 = float(input('Altitude of the target position:'))        # Altitude of the target position
+
+Matrix_NED_1 = np.matrix ([[ 1, 0, 0],
+                           [ 0, sin(Latitude1), cos(Latitude1)], 
+                           [ 0, -cos(Latitude1), cos(Latitude1)]])
+Matrix_NED_2 = np. matrix ([[ -sin(Longitude1), cos(Longitude1), 0],
+                            [ -cos(Longitude1), -sin(Longitude1), 0],
+                            [ 0, 0, 1]])
+
+NED_Rotation_Matrix = Matrix_NED_1 * Matrix_NED_2
