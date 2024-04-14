9115c48eae4b7638edb89e5a9b9f7c845ceabe7d
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-04-14 19:09:15 +0800

    Update LLA2ECEF.py

--- LLA2ECEF.py
+++ LLA2ECEF.py
@@ -3,9 +3,9 @@
 from math import sin, cos, sqrt
 
 
-Phi = float(input('Enter the Latitude of Location (Radians): \n'))
-Lambda = float(input('Enter the Longitude of Location (Radians): \n'))
-H = float(input('Enter the Altitude of Location (Meters): \n'))
+Latitude = float(input('Enter the Latitude of Location (Radians): \n'))
+Longitude = float(input('Enter the Longitude of Location (Radians): \n'))
+Altitude = float(input('Enter the Altitude of Location (Meters): \n'))
 
 a = 6378137                         # Semi-major Axis
 b = 6356752.314245                  # Semi-minor Axis
@@ -13,10 +13,10 @@ f = (a-b)/a                         # Inverse Flattening
 e = sqrt((a**2-b**2)/a**2)          # First Eccentricity
 e2 = f*(2-f)                        # First Eccentricity Squared
 
-R = a/sqrt(1-(e2*(sin(Phi)**2)))    # Radius of Curvature in the Prime Vertical
-X = (R+H)*cos(Phi)*cos(Lambda)
-Y = (R+H)*cos(Phi)*sin(Lambda)
-Z = ((1-e2)*R+H)*sin(Phi)
+R = a/sqrt(1-(e2*(sin(Latitude)**2)))    # Radius of Curvature in the Prime Vertical
+X = (R+Altitude)*cos(Latitude)*cos(Longitude)
+Y = (R+Altitude)*cos(Latitude)*sin(Longitude)
+Z = ((1-e2)*R+Altitude)*sin(Latitude)
 
 print ('Radius of Curvature: ', R)
 print ('X-Coordinate:', X)
