f721087d3360331728db899c897a10d879870cb5
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-05-05 14:17:51 +0800

    Update ECEF_2_NED.py

--- Direction_Cosine_Matrices/ECEF_2_NED.py
+++ Direction_Cosine_Matrices/ECEF_2_NED.py
@@ -5,12 +5,23 @@
 import numpy as np
 from math import sin, cos
 
-def ECEF_2_NED(Latitude, Longitude, Altitude):
+def ECEF_2_NED(X, Y, Z, Latitude, Longitude, Altitude):
   Matrix_NED_1 = np.matrix ([[ -sin(Latitude), 0, cos(Latitude)],
-                           [ 0, 1, 0], 
-                           [ -cos(Latitude), 0, -sin(Latitude)]])
+                             [ 0, 1, 0], 
+                             [ -cos(Latitude), 0, -sin(Latitude)]])
   Matrix_NED_2 = np. matrix ([[ cos(Longitude), sin(Longitude), 0],
-                            [ -sin(Longitude), cos(Longitude), 0],
-                            [ 0, 0, 1]])
+                              [ -sin(Longitude), cos(Longitude), 0],
+                              [ 0, 0, 1]])
   NED_Rotation_Matrix = Matrix_NED_1 * Matrix_NED_2
 
+  Coordinates_ECEF = np.matrix ([[ X ],
+                            [ Y ], 
+                            [ Z ]])
+
+  NED_convention = NED_Rotation_Matrix * Coordinates_ECEF
+
+  x = NED_convention[0][0] 
+  y = NED_convention[1][0] 
+  z = NED_convention[2][0] 
+  
+  return x, y, z
