115fef0f8d0095d8d9efe8ce37d7886a6ce6bb09
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-05-05 14:32:59 +0800

    Update LLA_2_ECEF.py

--- Coordinate_System_Transformation/LLA_2_ECEF.py
+++ Coordinate_System_Transformation/LLA_2_ECEF.py
@@ -1,1 +1,18 @@
+# Coordinate System Transformation 
+# Transforming the system from Latitude, Longitude and Altitude (LLA) to Earth-Centred, Earth-Fixed (ECEF)
+
+from math import sin, cos, sqrt
+
+def LLA_2_ECEF (Latitude, Longitude, Altitude):
+  a = 6378137                         # Semi-major Axis
+  b = 6356752.314245                  # Semi-minor Axis
+  f = (a-b)/a                         # Inverse Flattening
+  e = sqrt((a**2-b**2)/a**2)          # First Eccentricity
+  e2 = f*(2-f)                        # First Eccentricity Squared
+
+  X = ((a/(sqrt(1-e2*(sin(Latitude))**2)))+Altitude)*cos(Latitude)*cos(Longitude)
+  Y = ((a/(sqrt(1-e2*(sin(Latitude))**2)))+Altitude)*cos(Latitude)*sin(Longitude)
+  Z = (b/a)**2*((a/sqrt(1-e2*(sin(Latitude))**2)))+Altitude)*sin(Latitude) 
+
+return X, Y, Z
 
