fe94cac53608f58bcf8a293f8c2bc6bfce62edd5
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-05-15 22:47:08 +0800

    Update euler2quaternion.m

--- Coordinate_System_Transformation/euler2quaternion.m
+++ Coordinate_System_Transformation/euler2quaternion.m
@@ -1,4 +1,4 @@
-function [q0, q1, q2, q3] =  euler2quaternion (roll, pitch, yaw);
+function [q0, q1, q2, q3] =  euler2quaternion (roll, pitch, yaw)
 % Description: Converting the euler angles to quaternions
 % A quaternion is represented by four elements: q0+iq1+jq2+kq3, where q0, 
 % q1, q2 and q3 are real numbers, and i, j and k are mutually orthogonal 
