5dd3bdf1be1b2907d091787f6012e2d7b6c6fc8f
shermanteo <166577232+shermanteo@users.noreply.github.com>
2024-05-15 22:44:03 +0800

    Update quaternion2RotationMatrix.m

--- Coordinate_System_Transformation/quaternion2RotationMatrix.m
+++ Coordinate_System_Transformation/quaternion2RotationMatrix.m
@@ -1,4 +1,4 @@
-function R =  quaternion2RotationMatrix (q0, q1, q2, q3);
+function R =  quaternion2RotationMatrix (q0, q1, q2, q3)
 % Description: Converting the quaternions to a rotation matrix
 % A quaternion is represented by four elements: q0+iq1+jq2+kq3, where q0, 
 % q1, q2 and q3 are real numbers, and i, j and k are mutually orthogonal 
